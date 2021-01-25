e5653323741a0df54c10d2006428ebf85e476f10
Growtheye <59008182+Growtheye@users.noreply.github.com>
2021-01-25 12:12:06 +0530

    Add files via upload

---
+++ thankyou.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="HandheldFriendly" content="true" />
+        <link rel="shortcut icon" href="" type="image/x-icon" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&subset=latin,latin-ext" type="text/css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,300i,400,400i,700,700i&subset=latin,latin-ext" type="text/css" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300italic,400,400italic,700,700italic&subset=latin,latin-ext" type="text/css" rel="stylesheet" />
+        <!-- Bootstrap CSS -->
+        <link href="./assets/css/oldbootstrap.css" rel="stylesheet" />
+        <link href="./assets/css/base.css" type="text/css" rel="stylesheet" />
+        <!-- <link href="https://styles.assets-landingi.com/urU0v0MG/base.css" type="text/css" rel="stylesheet"> -->
+        <title>Dollar Business - Thank You</title>
+        <script>var youtubeVideos = [];</script>
+        <style>
+            body{
+                /*  */
+                background-image: url('./assets/images/Banner3.png') ;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+            }
+            #KHcQGpSCtkk64eBLTtI8BLqsr0ubFupK {
+                background-image: none;
+            }
+            #NNqJKLXZkk9lfstCurNacSNlktdVwv3E {
+                text-align: center;
+                font-size: 70px;
+                width: 100%;
+                margin-top: 50px;
+                /* top: 70%; */
+            }
+            #La8xebs0VVcCOrz77ZIEK9oKs8FfqAOE {
+                position: fixed;
+                display: block;
+                bottom: 0px;
+                width: 100%;
+            }
+            @media (max-width:767px) {
+                body {
+                    background-image: url('./assets/images/Banner3.png') ;
+                    -webkit-background-size: cover;
+                    -moz-background-size: cover;
+                    -o-background-size: cover;
+                    background-size: cover;
+                }
+                #KHcQGpSCtkk64eBLTtI8BLqsr0ubFupK {
+                    background-image: url('./assets/images/Banner3.png') ;
+                    height: 900px;
+                }
+                #NNqJKLXZkk9lfstCurNacSNlktdVwv3E{
+                    font-size: 62px;
+                    text-align: inherit;
+                    }
+            }
+        </style>
+    </head>
+    <body id="">
+        <div id="KHcQGpSCtkk64eBLTtI8BLqsr0ubFupK" class="widget widget-section">
+            <div class="row">
+                <div class="container">
+                    <div id="NNqJKLXZkk9lfstCurNacSNlktdVwv3E" class="widget widget-text text-center">
+                        <h1><font color="#ffffff">Thank YOU <br>We will get back to you soon.</font></h1>
+                    </div>
+                    <img id="wVhzPo1QPiZvqhTJG29xtbSGViSa4MSf" width="111" height="169" class="widget widget-image" data-original="./assets/images/Asset_1.png" src="./assets/images/Asset_1.png" alt="" />
+                </div>
+            </div>
+        </div>
+        <div id="La8xebs0VVcCOrz77ZIEK9oKs8FfqAOE" class="widget widget-section">
+            <div class="row">
+                <div class="container">
+                    <div id="VfU5duTTu8KOHrchGXx4hPEKIggdXMcW" class="widget widget-text"><p>Copyright © The Dollar Business, Designed by 8views</p></div>
+                </div>
+            </div>
+        </div>
+    </body>
+    <style>
+       
+    </style>
+</html>
